567eb55ae3829652b402881742d59f20fa63d67c
contenidos <contenidos@ACADEMIA.ES>
2018-04-17 13:39:13 +0200

    Aprendiendo grid...

--- assets/curso/js/iniciar.js
+++ assets/curso/js/iniciar.js
@@ -1,3 +1,11 @@
 var options = {};
 var elem = document.querySelector('.sidenav');
 var instance = M.Sidenav.init(elem, options);
+
+let iframe = document.querySelector('iframe');
+console.log(iframe.clientHeight)
+console.log(iframe.offsetHeight)
+console.log(iframe.scrollHeight)
+
+console.log(iframe.contentWindow.innerHeight)
+console.log(iframe.contentWindow.outerHeight)
