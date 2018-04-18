eade1d5653bac274bb5bb8b66bce9afdb5cc6272
contenidos <contenidos@ACADEMIA.ES>
2018-04-18 16:09:38 +0200

    probando pull

--- assets/curso/js/iniciar.js
+++ assets/curso/js/iniciar.js
@@ -3,17 +3,16 @@ var elem = document.querySelector('.sidenav');
 var instance = M.Sidenav.init(elem, options);
 
 let iframe = document.querySelector('iframe');
+let contenedor = document.querySelector('article');
 /* console.log(iframe.clientHeight);
 console.log(iframe.offsetHeight);
 console.log(iframe.scrollHeight); */
 
-console.log(iframe.contentWindow.document.body.scrollHeight);  // FUNCIONA BIEN
+/* console.log(iframe.contentWindow.document.body.scrollHeight); // FUNCIONA BIEN
 console.log(iframe.contentWindow.document.documentElement.scrollHeight);
 console.log(iframe.contentWindow.document.documentElement.offsetHeight);
 //iframe.contentWindow.document.documentElement.style.overflowY='hidden';
 //let altura = iframe.contentWindow.document.body.scrollHeight;
 let altura = iframe.contentWindow.document.body.scrollHeight;
-document.querySelector('article').height=altura;
-iframe.height = altura; 
-
-
+document.querySelector('article').height = altura;
+iframe.height = altura; */
