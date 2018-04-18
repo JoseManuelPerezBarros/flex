8859d83f470e59785daa642aaafb38ae3b6cfd7e
contenidos <contenidos@ACADEMIA.ES>
2018-04-18 09:54:05 +0200

    Avanzando con el iframe

--- assets/curso/js/iniciar.js
+++ assets/curso/js/iniciar.js
@@ -3,9 +3,17 @@ var elem = document.querySelector('.sidenav');
 var instance = M.Sidenav.init(elem, options);
 
 let iframe = document.querySelector('iframe');
-console.log(iframe.clientHeight)
-console.log(iframe.offsetHeight)
-console.log(iframe.scrollHeight)
+/* console.log(iframe.clientHeight);
+console.log(iframe.offsetHeight);
+console.log(iframe.scrollHeight); */
 
-console.log(iframe.contentWindow.innerHeight)
-console.log(iframe.contentWindow.outerHeight)
+console.log(iframe.contentWindow.document.body.scrollHeight);  // FUNCIONA BIEN
+console.log(iframe.contentWindow.document.documentElement.scrollHeight);
+console.log(iframe.contentWindow.document.documentElement.offsetHeight);
+//iframe.contentWindow.document.documentElement.style.overflowY='hidden';
+//let altura = iframe.contentWindow.document.body.scrollHeight;
+let altura = iframe.contentWindow.document.body.scrollHeight;
+document.querySelector('article').height=altura;
+iframe.height = altura; 
+
+
